7632c19cf7990a452a7085f1db8bcfc1008de825
Gabriel Espindola <zgabesp@xead.ericsson.com>
2025-06-10 23:24:31 -0300

    Adding anchor generation logic

--- model/rpn.py
+++ model/rpn.py
@@ -38,7 +38,7 @@ class RegionProposalNetwork(nn.Module):
                                         kernel_size=1,
                                         stride=1) 
     
-    def generate_anchors(self, image, feature_map):        
+    def generate_anchors(self, image: torch.Tensor, feature_map: torch.Tensor) -> torch.Tensor:        
         """Generate all anchors boxes in the image.\n
         The base anchors will be zero centered and are blueprint of the defined "scales" and "aspect_ratios" (in our case 9 anchor boxes around base anchors).\n
         The base anchors will be multiplicated, accordingly to scale and aspect ratios, and shifted using feature map stride to crate anchors for entire image.\n
@@ -56,25 +56,58 @@ class RegionProposalNetwork(nn.Module):
         scales = torch.tensor(self.scales, dtype=feature_map.dtype, device=feature_map.device)
         aspect_ratios = torch.tensor(self.aspect_ratios, dtype=feature_map.dtype, device=feature_map.device)
         
-        #===================================================
         #Getting anchor boxes of area 1 (h*w = 1)
-        #===================================================
         #The bellow code is only valid if h/w = aspect_ratios. Therefore, h = sqrt(aspect_ratios) and w = 1/h
         h = torch.sqrt(aspect_ratios) # height of unit area anchor boxes
         w = 1/h # width of unit area anchor boxes
         
         # Getting 1 area anchors with different scales
-        ws =(w[:, None] * scales[None, :]).view(-1) # get width of unit area anchors in all different scales
-        hs =(h[:, None] * scales[None, :]).view(-1) # get height of unit area anchors in all different scales
+        ws =(w[:, None] * scales[None, :]).view(-1) # get width of unit area anchors in all different scales (eg: 3x3 where lines are aspect ratios and columns are scales)
+        hs =(h[:, None] * scales[None, :]).view(-1) # get height of unit area anchors in all different scales (eg: 3x3 where lines are aspect ratios and columns are scales)
         
-        # give the width and height of all 9 anchor boxes 
-        # All those boxes are centered at (0,0) 
-        # eg: those base_anchors will be shape 9x4 
-        base_anchors = torch.stack([-ws, -hs, ws, hs], dim=1)/2 
+        # calculate width and height of all 9 anchor boxes 
+        # All those boxes are centered in the same point (0,0)
+        # eg: shape 9x4 where each line is an anchor and each colums is an edge
+        # [-91., -45., 91., 45.]
+        # [-181., -91., 181., 91.]
+        # [-362., -182., 362., 181.]
+        # [-64., -64., 64., 64.]
+        # [-128., -128., 128., 128.]
+        # [-256., -256, 256., 256.]
+        # [-45., -91., 45., 91.]
+        # [-91., -181. 91., 181.]
+        # [-181., -362., 181., 362.]
+        base_anchors = torch.stack([-ws, -hs, ws, hs], dim=1)/2  
         base_anchors =base_anchors.round()
         
-        # Get the shift in x axis
+        # Get shifts of (0,0) anchors to all locations in feature map in x axis -> (0, 1, ..., w_feat-1) * stride_w
+        # obs: we shift the anchors in feature map proportionally to the original image sizes, this explains the stride_w
+        shifts_x = torch.arange(0, grid_w, dtype=torch.int32, device=feature_map.device) * stride_w
+    
+        # Get shifts of (0,0) anchors to all locations in feature map in y axis -> (0, 1, ..., h_feat-1) * stride_h
+        # obs: we shift the anchors in feature map proportionally to the original image sizes, this explains the stride_h
+        shifts_y = torch.arange(0, grid_h, dtype=torch.int32, device=feature_map.device) * stride_h
 
+        # Do cartesian product between shifts to get anchor coordinates in feature map
+        # torch.meshgrid reference: https://docs.pytorch.org/docs/stable/generated/torch.meshgrid.html
+        shifts_y, shifts_x = torch.meshgrid(shifts_y, shifts_x, indexing="ij")
+
+        #flatten shifts coordinates
+        shifts_y = shifts_y.reshape(-1)
+        shifts_x = shifts_x.reshape(-1)
+        
+        # shift is 4 dimesion because the shift is applied in the 4 points of the box
+        #                   bottom_left top_left bottom_right top_right
+        shifts = torch.stack([shifts_x, shifts_y, shifts_x,  shifts_y], dim=1) # shifts -> (h_feat * w_feat, 4)
+        
+        # Adding shifts to original (0,0) base anchors and creating all anchor boxes to entire image
+        # base_anchors -> (num_anchors_per_location, 4) eg: 9x4
+        anchors = (shifts.view(-1,1,4) + base_anchors.view(1,-1,4)) # anchors -> (h_feat * w_feat, num_anchors_per_location, 4) 
+        anchors = anchors.reshape(-1, 4) # anchors -> (h_feat * w_feat * num_anchors_per_location, 4) eg: 16650x4
+        
+        #Note: In this implementation, the center of anchors is not in the middle of the cell, instead they are in top left corner of each cell
+        return anchors
+        
     def forward(self, image, feature_map, target):
         """Forward method for RPN
 
